3b0a37626f322a89b56b64d176cb72e96f3090b0
yogashri1126 <ypradhan.pge@texas.edu>
2017-07-17 20:07:06 -0500

    trying to get tally to work, yet to conver this to harder homework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,23 +29,84 @@ $("#start").on("click", function() {
 
 countRight=0;
 countWrong=0;
-var submitAnswer = function() {
+
+$("#SubmitOne").on("click", function(){
 
   var radios = $('.choice');
   var val= "";
   for (var i = 0, length = radios.length; i < length; i++) {
+  	console.log(radios)
       if (radios[i].checked) {
          val = radios[i].value; 
-         break;
-       }
-  }
+         if (val === "Titusville")
+         { 
+    		countRight++
+    	}
+
+    	console.log(countRight)
+
+    	if(val==="August" ){
+    		countRight++}
+
+    	if(val=== "Right")
+    	{
+    		countRight++
+    	}
+  		
+  		if(val=== "Correct")
+  		{
+  			countRight++
+  		}
+
+  		if(val=== "ExxonMobil")
+  		{
+  			countRight++
+  		}
+
+  		if(val=== "Janeen")
+  		{
+  			countRight++
+  		}
+
+  		if(val=== "Accurate")
+  		{
+  			countRight++
+  		}
+
+  		if(val==="USA")
+  		{
+  			countRight++
+  		}
+
+  		if(val === "Venezuela")
+  		{
+  			countRight++
+  		}
+
+  		if(val === "unconventional")
+  		{
+  			countRight++
+  		}
+
+  		if(val === "Old")
+  		{
+  			countRight++
+  		}
+
+  			else {
+    				countWrong++
+  					}
+        			 
+       				}
+ 			 }
+    	
+    	
+
   
-  if (val === "Titusville" ) {
-    countRight++}
-  else {
-    
-    countWrong++
-  }
-};
+  
+
+alert("You've got" + " "+ countRight + " "+ "correct" + " "+"and"+ " "+ countWrong + " " + "incorrect!")
+
+});
 
 })
